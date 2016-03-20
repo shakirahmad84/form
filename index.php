ebfed8c59684ef4430ae378ecc087635f0fefb8c
Shakir Ahmad <shakirahmad84@users.noreply.github.com>
2016-03-20 12:31:43 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert.php">
+        <p>Add Your Comment:</p>
+        <textarea name="comment" cols="30" rows="5"></textarea><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
