0c0028ede6212f37223594e5245fb3ee2cd12bf8
Shakir Ahmad <shakirahmad84@users.noreply.github.com>
2016-03-21 16:38:47 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "form";
-// Create Connection
-$con = mysqli_connect($servername, $username, $password, $dbname);
-// Check Connection
-if (!$con){
-    die("could not connect");
-}
-else {
-    echo "Connected.";
+$sql = $_POST['name'];
+
+if(mysqli_query(mysqli_connect("localhost", "root", ""), "create database $sql")){
+    echo "Database Created Successfully.";
 }
